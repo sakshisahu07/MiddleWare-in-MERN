ed967cf9cfbdc913c0d423de365099c5190d4945
sakshisahu07 <sakshisahu072@gmail.com>
2024-09-21 13:44:52 -0700

    routes

--- index.js
+++ index.js
@@ -1,58 +1,12 @@
-// const express=require("express");
-// const app=express();
-
-// app.get("/home",(req,res)=>{
-//     res.send("<h1>WE are MERN Developer</h1>");
-// })
-
-// app.get("/about",(req,res)=>{
-//     res.send("<h1>All About us!!</h1>");
-// })
-// app.get("/contact",(req,res)=>{
-//     res.send("<h1>Contact us</h1>");
-// })
-// app.get("/service",(req,res)=>{
-//     res.send("<h1>Contact us</h1>");
-// })
-
-// app.listen(8000);
-
-
-// const express=require("express");
-// const app=express();
-// const PORT=8000;
-
-// app.get("/",(req,res)=>{
-//     res.send("Welcome to my program!!!");
-// })
-
-// app.listen(PORT,()=>{
-//     console.log(`server Run on port:${PORT}`);
-// })
-
-
-// const express=require("express");
-// const app=express();
-// const stuRoute=require("./routes/studentRoutes");
-// const teacherRoute=require("./routes/teacherRoutes");
-// app.get("/",(req,res)=>{
-//     res.send("Welcome to Home page");
-// })
-// app.use("/students",stuRoute);
-// app.use("/teacher",teacherRoute);
-
-// app.listen (8000,()=>{
-//     console.log("Server runs on this port");
-// });
 
 const express=require("express");
 const app=express();
 const stuRoute=require("./routes/studentRoutes")
 app.get("/",(req,res)=>{
-    res.send("<h1>This is my home page");
+    res.send("<h1>Welcome to home page");
 })
 app.use("/student",stuRoute);
 
 app.listen(8000,()=>{
-    console.log("server run on this port");
+    console.log("server runs on this port");
 })
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,25 +1,11 @@
 
-// const express=require("express");
-// const route=express.Router();
-
-// route.get("/stuinfo",(req,res)=>{
-//     res.send("<h1>Students information");
-// })
-// route.get("/stufees",(req,res)=>{
-//     res.send("<h1>Students fees");
-// })
-// route.get("/result",(req,res)=>{
-//     res.send("<h1>Student Result");
-// })
-// module.exports=route;
-
 const express=require("express");
 const route=express.Router();
 
 route.get("/stuinfo",(req,res)=>{
-    res.send("<h1>students information");
+    res.send("<h1>Students information");
 })
 route.get("/stufees",(req,res)=>{
-    res.send("<h1>students Fees");
+    res.send("<h1>student fees records");
 })
 module.exports=route;
