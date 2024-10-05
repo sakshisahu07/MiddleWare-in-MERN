c4083e8a2b78a195b56cafcdc550d8d385db19e2
sakshisahu07 <sakshisahu072@gmail.com>
2024-10-05 14:34:02 -0700

    MiddleWare

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express=require("express");
 const app=express();
-const stuRoute=require("./routes/studentRoute")
-const mongoose=require("mongoose")
-var bodyParser = require('body-parser')
 
-mongoose.connect("mongodb://127.0.0.1:27017/Employee").then(()=>{
-    console.log("DB Successfully connected")
+app.use("/",(req,res,next)=>{
+    console.log("this is a middle ware which is alwysss runsss")
+    next();
 })
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.get("/",(req,res,next)=>{
+    console.log("home console");
+    res.send("Home page");
+    next();
+})
 
-// parse application/json
-app.use(bodyParser.json()) 
+app.get("/about",(req,res,next)=>{
+    console.log("about Page");
+    res.send("About uss!!1")
+})
 
+app.use("/",(req,res,next)=>{
+    console.log("Millde Ware 1");
+    next();
+})
 
-app.use("/employee",stuRoute);
 app.listen(8000,()=>{
     console.log("Server Runs on this port");
+
 })
--- models/studentModel.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose=require("mongoose");
-const empSchema=new mongoose.Schema({
-    empno:Number,
-    name:String,
-    city:String,
-    salary:Number
-})
-module.exports=mongoose.model("employee",empSchema);
